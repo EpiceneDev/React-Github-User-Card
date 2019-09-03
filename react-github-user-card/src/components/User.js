aea86560fda46775f2cde69d9a97dc45f7412437
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-03 11:38:34 -0700

    CCR frame built

---
+++ react-github-user-card/src/components/User.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class User extends React.Component {
+    // constructor() {
+    //     super();
+    //     state
+    // }
+    state = {
+        users: []
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
+
+export default User;
