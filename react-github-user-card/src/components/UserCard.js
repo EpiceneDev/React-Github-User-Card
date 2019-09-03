aeacee954b96dae3f0f058c77d90dc5024240b34
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-09-03 14:17:32 -0700

    Bootstrap attempt

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
+
 function UserCard(props) {
-    
     return (
-        <div className="user">
-            <h1>Github Users</h1>
-            <h2>{props.user.login}</h2>
-            <p>{props.user.location}</p>
-            <p>{props.user.url}</p>
-            {props.followers.map(follower => (
-                <div key={follower.id}>{follower.login}</div>
-            ))}
+        <div class="card mb-3" >
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="..." class="card-img" alt="not avail" />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h3 class="card-title">{props.user.login}</h3>
+                        <p class="card-text">Location: {props.user.location || "unknown"}</p>
+                        <p>Followers: 
+                        {props.followers.map(follower => (
+                            <div key={follower.id}>{follower.login}</div>
+                        ))}
+                        </p>
+                        <p class="card-text"><small class="text-muted">{props.user.url}</small></p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
-}
+};
+    
 
 export default UserCard;
