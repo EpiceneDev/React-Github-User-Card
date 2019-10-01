38dc4676c8ceebf68e8e1f04a90929934e647839
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-01 15:55:12 -0700

    oops. first save from mvp

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { tomato } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
     card: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
       minWidth: 275,
+      maxWidth: 275,
+      borderRadius: 30
     },
     bullet: {
       display: 'inline-block',
